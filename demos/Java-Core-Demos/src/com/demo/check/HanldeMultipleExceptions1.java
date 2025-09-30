cbb64c24a537eb775585c69fba2c4cb99ab7ac41
DEV <DEV@DEV.COM>
2025-09-30 14:34:12 +0530

    commit

---
+++ demos/Java-Core-Demos/src/com/demo/check/HanldeMultipleExceptions1.java
@@ -0,0 +1,29 @@
+package com.demo.check;
+
+import java.lang.ArrayIndexOutOfBoundsException;
+
+class Calculator1 {
+    public  void add(int num1, int num2) { System.out.println(num1 + num2);}
+} 
+
+
+public class HanldeMultipleExceptions1 {
+    public static void main(String[] args) {
+        String colors[] = {"Red", "Green", "Blue"};
+        Calculator1 calculator1 = null;
+        System.out.println("Normal Calculator");
+        try {
+            for (int i = 0; i <= 3; i--) {
+                System.out.println(colors[i]);
+            }
+        } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
+            try {
+                calculator1.add(50, 50);
+            } catch (NullPointerException nullPointerException) {
+                System.out.println("Instantiate the calculator....");
+            }
+            System.out.println("Check index...");
+        }
+        System.out.println("Thankyou!");
+    }
+}
